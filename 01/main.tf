981fd4d26d04019dfe32f860b1b3a39d54398683
AjinkyaP-09 <pameajinkya09@gmail.com>
2025-06-14 11:06:52 +0000

    01

--- 01/main.tf
+++ 01/main.tf
@@ -1,9 +1,10 @@
-provider "aws" {
-  region= "ap-south-1"
+provider "aws"{
+  region = "ap-south-1"
 }
 
 resource "aws_instance" "Terra-Instance-1" {
-  ami = "ami-0f918f7e67a3323f0"
+  ami = "ami-02521d90e7410d9f0"
+  subnet_id = "subnet-05937d3cbd349f76b"
+  key_name = "demo"
   instance_type = "t2.micro"
 }
-
