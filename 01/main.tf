8d841e1e6608c98f360c5063e78bc9f3bee0ccc6
AjinkyaP-09 <pameajinkya09@gmail.com>
2025-06-14 10:29:10 +0000

    Add name to AWS instance resource

--- 01/main.tf
+++ 01/main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region= "ap-south-1"
 }
 
-resource "aws_instance" {
+resource "aws_instance" "Terra-Instance-1" {
   ami_id = "ami-0f918f7e67a3323f0"
   instance_type = "t2.micro"
 }
