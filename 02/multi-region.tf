52f938a532710fba1b3c3720ea077a92a6bd5ff0
AjinkyaP-09 <pameajinkya09@gmail.com>
2025-06-15 05:55:46 +0000

    Add 02/multiregion.tf

---
+++ 02/multi-region.tf
@@ -0,0 +1,29 @@
+provider "aws" {
+  alias = "region_1"
+  region = "ap-south-1"
+}
+
+provider "aws" {
+  alias = "region_2"
+  region = "us-east-1"
+}
+
+resource "aws_instance" "instancs-1"{
+  ami = "ami-0b09627181c8d5778"
+  instance_type = "t2.micro"
+  provider = "aws.region_1"
+  key_name = "demo"
+  tags = {
+    Name = "Instance-in-AP"
+  }
+}
+
+resource "aws_instance" "instancs-1"{
+  ami = "ami-09e6f87a47903347c"
+  instance_type = "t2.micro"
+  provider = "aws.region_2"
+  key_name = "demo"
+  tags = {
+    Name = "Instance-in-US"
+  }
+}
