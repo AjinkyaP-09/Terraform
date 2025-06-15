2a32dd629328ac666b543902a46d73251678b974
AjinkyaP-09 <pameajinkya09@gmail.com>
2025-06-15 06:11:28 +0000

    Merge

--- 02/multi-region.tf
+++ 02/multi-region.tf
@@ -8,7 +8,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "instancs-1"{
+resource "aws_instance" "instance-1"{
   ami = "ami-0b09627181c8d5778"
   instance_type = "t2.micro"
   provider = "aws.region_1"
@@ -18,7 +18,7 @@ resource "aws_instance" "instancs-1"{
   }
 }
 
-resource "aws_instance" "instancs-1"{
+resource "aws_instance" "instance-2"{
   ami = "ami-09e6f87a47903347c"
   instance_type = "t2.micro"
   provider = "aws.region_2"
